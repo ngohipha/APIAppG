67579566163ec3eab6c35ca636ceb7ea0422c53b
ngohipha <ngohipha@gmail.com>
2023-02-27 18:00:14 +0700

    add tojsion Id

--- models/category.model.js
+++ models/category.model.js
@@ -17,6 +17,19 @@ const category = mongoose.model(
             categoryImage:{
                 type: String,
             }
+        },
+        {
+            // mongodb trar veef id gach duoi khoa chinh bat ky mo hinh 
+            //  chuyen doi ham json tra ve id
+            //  delete xoa _id thay vao id doanh muc 
+            // delete _v nhan duoc bien theo yeu cau 
+            toJSON:{
+                transform: function (doc,ret){
+                    ret.categoryId = ret._id.toString();
+                    delete ret._id;
+                    delete ret._v;
+                }
+            }
         }
     )
 );
